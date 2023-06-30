1ee6785c019f9acec6116078cedf5e755d4917a6
Bheemarao Menda <Bheemarao.M@sstech.us>
2023-06-30 18:29:48 +0530

    Date format changed

--- src/test/java/com/pom/NewAppointmentPage.java
+++ src/test/java/com/pom/NewAppointmentPage.java
@@ -177,7 +177,7 @@ public class NewAppointmentPage {
         logger.log(Status.PASS,"Appointment Table");
         Thread.sleep(3000);
         appointmentDate.clear();
-        appointmentDate.sendKeys("30-06-2023");
+        appointmentDate.sendKeys("30/06/2023");
 
         Thread.sleep(2000);
         appointmentStartTime.click();
